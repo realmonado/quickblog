1f65e9cf635b89d0bcc0ec1fcae5d264cf623db0
monado <mannanshukla@protonmail.com>
2020-11-08 06:22:17 -0500

    thanks unowe lol, fixed linux specific bug

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ read filename
 cp template.md ${filename}.md
 echo "what should the title be?"
 read title
-echo "\n ### $(date +%m/%d/%Y)" >> ${filename}.md
+echo -e "\n ### $(date +%m/%d/%Y)" >> ${filename}.md
 pandoc -s ${filename}.md -o ${filename}.html --metadata title="${title}"
-echo "\n [$title](${filename}.html)" >> index.md
+echo -e "\n [$title](${filename}.html)" >> index.md
 pandoc -s index.md -o index.html --metadata title="quickblog demo"
